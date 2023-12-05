891bceee94903224a36081898aedf82c314eab94
chase <chase.delanguillette@gmail.com>
2023-12-05 02:03:49 -0800

    fix up art not rendering correctly

--- Character.cpp
+++ Character.cpp
@@ -32,60 +32,58 @@ void Character::setState(CharacterState newState) {
   frameCounter = 0;
   state = newState;
   switch (state) {
-      case IDLE:
-          currentSprite = idleSprite;
-          frameCount = frameCount_Idle;
-          frameChangeInterval = frameChangeInterval_Idle;
-          break;
-      case WALK:
-          currentSprite = walkSprite;
-          frameCount = frameCount_Walking;
-          frameChangeInterval = frameChangeInterval_Walking;
-          break;
-      case JUMP:
-          currentSprite = jumpSprite;
-          frameCount = frameCount_Jump;
-          frameChangeInterval = frameChangeInterval_Jump;
-          break;
-      case FALL:
-          currentSprite = fallSprite;
-          frameCount = frameCount_Fall;
-          frameChangeInterval = frameChangeInterval_Fall;
-          break;
-
+    case IDLE:
+      currentSprite = idleSprite;
+      frameCount = frameCount_Idle;
+      frameChangeInterval = frameChangeInterval_Idle;
+      break;
+    case WALK:
+      currentSprite = walkSprite;
+      frameCount = frameCount_Walking;
+      frameChangeInterval = frameChangeInterval_Walking;
+      break;
+    case JUMP:
+      currentSprite = jumpSprite;
+      frameCount = frameCount_Jump;
+      frameChangeInterval = frameChangeInterval_Jump;
+      break;
+    case FALL:
+      currentSprite = fallSprite;
+      frameCount = frameCount_Fall;
+      frameChangeInterval = frameChangeInterval_Fall;
+      break;
   }
 }
 
 void Character::startJump() {
   if (state != JUMP) {
-      velocityY = jumpPower; // Negative value for upward movement
-      setState(JUMP);
-    }
+    velocityY = jumpPower;  // Negative value for upward movement
+    setState(JUMP);
+  }
 }
 
 void Character::draw(Arduboy2 &arduboy) {
 
-    switch (state) {
-        case IDLE:
-            break;
-        case WALK:
-            break;
-        case JUMP:
-          y += velocityY; // Move the character up or down
-          velocityY += gravity; // Apply gravity
-          if (y >= groundLevel) { // Check if character lands
-            y = groundLevel; // Reset position to ground
-            setState(WALK);
-          }
-          break;
-        case FALL:
-          y += velocityY;
-          velocityY += gravity;
-          break;
-    }
-  
-    if (currentSprite != nullptr) {
-      Sprites::drawSelfMasked(x, y, currentSprite, currentFrame);
-    }
-}
+  switch (state) {
+    case IDLE:
+      break;
+    case WALK:
+      break;
+    case JUMP:
+      y += velocityY;          // Move the character up or down
+      velocityY += gravity;    // Apply gravity
+      if (y >= groundLevel) {  // Check if character lands
+        y = groundLevel;       // Reset position to ground
+        setState(WALK);
+      }
+      break;
+    case FALL:
+      y += velocityY;
+      velocityY += gravity;
+      break;
+  }
 
+  if (currentSprite != nullptr) {
+    Sprites::drawSelfMasked(x, y, currentSprite, currentFrame);
+  }
+}
