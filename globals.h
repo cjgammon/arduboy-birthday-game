57fce64c99db802e11d27ae0aa16413bfb457696
cjgammon <cj.gammon@gmail.com>
2023-12-05 23:18:18 -0600

    remove globals

--- globals.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARDUBOY_BIRTHDAY_GAME_GLOBALS_H
-#define ARDUBOY_BIRTHDAY_GAME_GLOBALS_H
-
-#include "Character.h"
-
-extern Character* PlayerCharacter;
-
-#endif //ARDUBOY_BIRTHDAY_GAME_GLOBALS_H
