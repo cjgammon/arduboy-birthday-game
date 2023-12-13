02563381f2eb4a28188cbfef8532b337f7cba248
chase <chase.delanguillette@gmail.com>
2023-12-13 09:56:20 -0800

    rework godMode into cheatMode, with separate options for disabling falling, collisions, or both

--- GameUI.h
+++ GameUI.h
@@ -26,7 +26,6 @@ private:
     void drawScore(Arduboy2 &arduboy);
     void drawLives(Arduboy2 &arduboy);
     void drawSpeed(Arduboy2 &arduboy);
-    void drawGodMode(Arduboy2 &arduboy);
 };
 
 #endif // GAME_UI_H
