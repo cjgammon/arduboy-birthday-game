8e5d60921c9f0e8c5ca977f6346010f0b6999c1c
cjgammon <cj.gammon@gmail.com>
2023-12-18 01:45:27 -0600

    refactor and move createground to ground entity manager

--- EntityManager_Ground.h
+++ EntityManager_Ground.h
@@ -2,6 +2,7 @@
 #define ENTITYMANAGER_GROUND_H
 
 #include "Entity_Ground.h"
+#include "SegmentDefinition.h"
 #include "Vars.h"
 
 class EntityManager_Ground {
@@ -12,19 +13,21 @@ public:
     void draw(); // Draw all entities
     void cleanup();
 
-    void addEntity(Entity* entity);
     bool isGroundAt(int x); // Check if there is ground at the given coordinates
     Entity_Enemy* enemyCollision(int playerX, int playerY, int playerRadius);
     Coin* coinCollision(int playerX, int playerY, int playerRadius);
 
-    static const int MAX_ENTITIES = 10; // Maximum number of entities (adjust as needed)
+    static const int MAX_ENTITIES = 3; // Maximum number of entities (adjust as needed)
     Entity* entities[MAX_ENTITIES]; // Store all entities
     int numEntities; // Number of currently added entities
-    int calculateDifficultyLevel();
 
 private:
+    void createGroundEntities();
+    void addEntity(Entity* entity);
     void recycleGroundEntity(int index);
     int findMaxRightX();
+    int calculateDifficultyLevel();
+    const SegmentDefinition* selectSegmentBasedOnDifficulty();
 };
 
 #endif // ENTITYMANAGER_GROUND_H
